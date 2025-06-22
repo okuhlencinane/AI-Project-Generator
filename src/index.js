e6abea874331329c4b861540a1fbc43f6d8c87c4
okuhlencinane <okuhlencinane1@gmail.com>
2025-06-22 21:53:54 +0200

    made changed in JS file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
-function buttonAction(event){
-    event.preventDefault();
-   
-      new Typewriter("#poem", {
-    strings: "It is never too late to be what you might have been.Change your thoughts and you change your world.",
+function showPoem(response){
+   new Typewriter("#poem", {
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
 }
 
-let instructionsButton =document.querySelector('.submit-button');
-instructionsButton.addEventListener('click',buttonAction);
+
+function buttonAction(event){
+
+    event.preventDefault();
+   let instructionsInput=document.querySelector("#poem-generator-form")
+   let apiKey ='37o01tf961eb43e8aa46dbf5fa9e5225';
+   let context=
+   'You are a widsom filled expert and love to give people motivation when they are confused and feeling depressed. Your mission is to generate a short 4 line poem based on User instructions below , make it short and simple.The poem must have </br> in between.Make sure to follow the user instructions. Do not include a title to the poem.';
+   let prompt=`User instructions: Generate a motivation poem about ${instructionsInput.value} ';
+   let apiUrl='https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}';
+
+  axios.get(apiURL).then(showPoem);
+}
+  let poemFormElement = document.querySelector("#poem-generator-form");
+  poemFormElement.addEventListener("submit", buttonAction);
