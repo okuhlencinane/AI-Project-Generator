1016062bd00ce48782dc23d679e7262bf00329fe
okuhlencinane <okuhlencinane1@gmail.com>
2025-09-13 18:16:27 +0200

    made css changes

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ function buttonAction(event) {
     event.preventDefault();
     let instructionsInput = document.querySelector("#poem-generator-form input");
     //let apiKey = '37o01tf961eb43e8aa46dbf5fa9e5225';
-    let context = "You are a wisdom-filled expert and love to give people information about great poems. Your mission is to generate a short 6-line poem based on User instructions below use the first word user is inserting, make it short and simple. The poem must have </br> in between. Make sure to follow the user instructions.";
+    let context = "You are a poem expert and love to give people amazing and great poems. Your mission is to generate a short 6-line poem based on User instructions below use the first word user is inserting, make it short and simple amazing. The poem must have </br> in between. Make sure to follow the user instructions.";
 
     let prompt = 'User instructions: Generate an Africa beauty poem about ${instructionsInput.value}'; 
     let apiURL = 'https://api.shecodes.io/ai/v1/generate?prompt=Generate a poem about Loving life  ${instructionsInput.value}&context=You are a wisdom-filled expert and love to give people information about poems. Your mission is to generate a short 6-line poem based on User instructions below, make it short and simple.Make sure to follow the user instructions.&key=37o01tf961eb43e8aa46dbf5fa9e5225';
@@ -24,3 +24,6 @@ function buttonAction(event) {
 
 let poemFormElement = document.querySelector("#poem-generator-form");
 poemFormElement.addEventListener("submit", buttonAction);
+
+const theme = localStorage.getItem("theme") || "dark";
+document.body.classList.add(theme);
